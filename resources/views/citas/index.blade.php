3de75607dc63cbcf5e1f441f3b0866c5ed8a2e41
Jaaziel2345 <jaazielmc45@gmail.com>
2023-09-15 10:07:53 -0600

    Validacion de input de tipo numerico

--- resources/views/citas/index.blade.php
+++ resources/views/citas/index.blade.php
@@ -32,15 +32,32 @@
 @push('js_scripts')
     <script src="{{asset('js/cita.js')}}"></script>
     <script>
-        //Validaciones de input
-        var cleave = new Cleave('#celular', {
+    // Validaciones de input
+    var celularCleave = new Cleave('#celular', {
         delimiter: '-',
-        blocks: [4,4],
-        });
-        //Validaciones de input dui
-        var cleave = new Cleave('#dui', {
+        blocks: [4, 4],
+    });
+
+    var duiCleave = new Cleave('#dui', {
         delimiter: '-',
-        blocks: [8,1],
-        });
-    </script>
+        blocks: [8, 1],
+    });
+
+    // Función para validar números
+    function validarNumeros(event) {
+        var input = event.target;
+        var valor = input.value;
+
+        // Mantener solo dígitos y guiones
+        valor = valor.replace(/[^\d-]/g, '');
+
+        input.value = valor;
+    }
+
+    // Agregar evento de escucha para validar números
+    document.getElementById('celular').addEventListener('input', validarNumeros);
+    document.getElementById('dui').addEventListener('input', validarNumeros);
+</script>
+
+
 @endpush
