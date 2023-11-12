a1aad68d3568d29e8836f68df94c69dadfe1266e
Jaaziel2345 <jaazielmc45@gmail.com>
2023-11-11 21:25:14 -0600

    Redireccion en sidebar a graficos

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -22,6 +22,13 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+               <li class="nav-item">
+            <a href="{{route('home')}}" class="nav-link">
+              <p class="text-white">
+                Dashboard
+              </p>
+            </a>
+          </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-calendar-alt"></i>
